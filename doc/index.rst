3c468457e34414cecb289dc51eeadd10606ac0e8
Liam Keegan <liam@keegan.ch>
2021-06-18 21:09:32 +0200

    add readme

--- doc/index.rst
+++ doc/index.rst
@@ -1,10 +1,18 @@
 pybind11-numpy-example
-==============================
+======================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-This is an example function:
+What
+----
 
-.. doxygenfunction:: pybind11numpyexample::add_one
+A simple example of how to use pybind11 with NumPy.
+
+This python library simply returns a vector of ints, either as a Python List or as a NumPy array.
+
+Why
+---
+
+For many use cases, Python Lists are slow to access and use a lot of memory compared to a NumPy array.
--- python/pybind11-numpy-example_python.cpp
+++ python/pybind11-numpy-example_python.cpp
@@ -17,12 +17,12 @@ static std::vector<short> vector_as_list(std::size_t size) {
   return make_vector<short>(size);
 }
 
-/** @brief Return a vector as a Numpy Array
+/** @brief Return a vector as a NumPy array
  *
  * Makes a copy of an existing vector of data
  *
  * @param size The size of the vector to return
- * @returns the vector as a Numpy Array
+ * @returns the vector as a NumPy array
  */
 static py::array_t<short> vector_as_array(std::size_t size) {
   auto temp_vector = make_vector<short>(size);
@@ -33,7 +33,7 @@ PYBIND11_MODULE(pybind11numpyexample, m)
 {
   m.doc() = "Python Bindings for pybind11-numpy-example";
   m.def("vector_as_list", &vector_as_list, "Returns a vector of 16-bit ints as a Python List");
-  m.def("vector_as_array", &vector_as_array, "Returns a vector of 16-bit ints as a Numpy Array");
+  m.def("vector_as_array", &vector_as_array, "Returns a vector of 16-bit ints as a NumPy array");
 }
 
 
