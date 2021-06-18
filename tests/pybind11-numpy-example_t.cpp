316a2ebb682a1201f3a6b8a89859c6584134525a
Liam Keegan <liam@keegan.ch>
2021-06-18 20:54:57 +0200

    fix broken test

--- tests/pybind11-numpy-example_t.cpp
+++ tests/pybind11-numpy-example_t.cpp
@@ -4,6 +4,8 @@
 using namespace pybind11numpyexample;
 
 TEST_CASE( "make_vector", "[make_vector]" ){
-  REQUIRE(make_vector<int>(0) == std::vector<int>{0});
+  REQUIRE(make_vector<int>(0) == std::vector<int>{});
   REQUIRE(make_vector<int>(5) == std::vector<int>{0, 1, 2, 3, 4});
+  REQUIRE(make_vector<short>(3) == std::vector<short>{0, 1, 2});
+  REQUIRE(make_vector<std::size_t>(4) == std::vector<std::size_t>{0, 1, 2, 3});
 }
