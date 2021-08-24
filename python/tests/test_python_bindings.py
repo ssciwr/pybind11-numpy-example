b68174940274b8cf2a70262bca8d57d14d5818b2
Liam Keegan <liam@keegan.ch>
2021-08-24 10:11:04 +0200

    add pre-commit config
    - run pre-commit on all files
    - enable pre-commit.ci to check&fix PRs

--- python/tests/test_python_bindings.py
+++ python/tests/test_python_bindings.py
@@ -1,6 +1,7 @@
 import pybind11numpyexample
 import numpy as np
 
+
 def test_pybind11_numpy_example():
     assert pybind11numpyexample.vector_as_list(0) == []
     assert pybind11numpyexample.vector_as_list(3) == [0, 1, 2]
